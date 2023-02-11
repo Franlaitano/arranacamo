415c58bce6bdbcc7f4af17b3b50853fc07dc416e
agustinalvarino <agustinalvarinoalonso@gmail.com>
2023-02-11 18:05:17 -0300

    Admin renewal

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,14 @@
  * Colocar aquí JS "propio".
  * Notar que este código se ejecutará en el navegador.
  */
-const { Article } = require("../models");
-const articles = await Article.findAll();
 
-for (const article of articles) {
-  const editButton = document.getElementById(`editButton${article.id}`);
-  editButton.addEventListener("click", () => {
-    alert(`La concha de tu madre All Boys y artículo ${article.id}`);
-  });
-}
+// ---------------------- BOTONES ADMIN EDIT ------------------------------ //
+//const { Article } = require("../models");
+//const articles = await Article.findAll();
+
+//for (const article of articles) {
+//  const editButton = document.getElementById(`editButton${article.id}`);
+//  editButton.addEventListener("click", () => {
+//    alert(`La concha de tu madre All Boys y artículo ${article.id}`);
+//  });
+//}
