03792d571b9534610b57e2b32ee29bdaa3db647f
agustinalvarino <agustinalvarinoalonso@gmail.com>
2023-02-11 17:28:49 -0300

    Admin view without buttons

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,12 @@
  * Colocar aquí JS "propio".
  * Notar que este código se ejecutará en el navegador.
  */
+const { Article } = require("../models");
+const articles = await Article.findAll();
+
+for (const article of articles) {
+  const editButton = document.getElementById(`editButton${article.id}`);
+  editButton.addEventListener("click", () => {
+    alert(`La concha de tu madre All Boys y artículo ${article.id}`);
+  });
+}
--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -1,8 +1,21 @@
 const { roundToNearestMinutes } = require("date-fns");
+const { id } = require("date-fns/locale");
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
+const { Article } = require("../models");
+
 // Rutas relacionadas al panel de control (Admin):
 // ...
-router.get("/", adminController.index);
+
+router.get("/admin", async function (req, res) {
+  const articles = await Article.findAll();
+  res.render("admin", { articles });
+});
+
+router.post("/admin/eliminar/:id", async function (req, res) {
+  const articles = await Article.findAll();
+  alert("puto");
+  res.redirect("/admin");
+});
 module.exports = router;
